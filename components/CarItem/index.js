2907aec71d5e0305460d8d24d852200375a95c4d
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-17 13:09:48 +0530

    update: btn component done

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground } from "react-native";
 import React from "react";
 import styles from "./styles";
+import StyledButton from "../StyledButton";
 
 export default function CarItem() {
   return (
@@ -13,6 +14,22 @@ export default function CarItem() {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subTitle}>Starting at ₹54,65,000</Text>
       </View>
+
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Custom order was pressed");
+        }}
+      />
+
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("Existing Inventory was pressed");
+        }}
+      />
     </View>
   );
 }
